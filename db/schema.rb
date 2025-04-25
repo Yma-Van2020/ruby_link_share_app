e48fe8ff7717c5ddc80fe94404428f29c761d828
Yucong Ma <candicema2018@gmail.com>
2025-04-25 07:56:02 -0700

    configured user functionality and updated link's index file

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,10 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_24_184440) do
   create_table "links", force: :cascade do |t|
     t.string "title"
     t.string "url"
+
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+
     t.integer "user_id"
     t.index ["user_id"], name: "index_links_on_user_id"
   end
@@ -26,8 +28,10 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_24_184440) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
